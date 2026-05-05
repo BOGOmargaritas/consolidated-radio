893c48b84da38c7034422e168bb5645672cdd96d
jbell1995 <jbell1995@live.com>
2026-05-05 09:06:58 -0400

    added ‘published’ filter

--- src/content.config.ts
+++ src/content.config.ts
@@ -1,20 +1,23 @@
-import { defineCollection } from 'astro:content';
-import { z } from 'astro/zod';
-import { glob } from 'astro/loaders';
+import { defineCollection } from "astro:content";
+import { z } from "astro/zod";
+import { glob } from "astro/loaders";
 
 const episodes = defineCollection({
-  loader: glob({ base: './src/content/episodes', pattern: '**/*.json' }),
+  loader: glob({ base: "./src/content/episodes", pattern: "**/*.json" }),
   schema: z.object({
     number: z.number(),
     title: z.string(),
     description: z.string(),
     audioSrc: z.string(),
     image: z.string(),
-    tracks: z.array(z.object({
-      artist: z.string(),
-      title: z.string(),
-    })),
-    date: z.string()
+    tracks: z.array(
+      z.object({
+        artist: z.string(),
+        title: z.string(),
+      }),
+    ),
+    date: z.string(),
+    published: z.boolean().default(false),
   }),
 });
 
